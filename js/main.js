e638006702407f37ac88b992aaab965f524b2bdd
EGL-dev <emanueleonardog@gmail.com>
2020-09-30 13:55:18 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+$(document).ready(function(){
+
+
+
+$('body').scrollspy({target: ".navbar-nav", offset: 50});
+
+
+$(".navbar-nav a").on('click', function(event) {
+
+    $(".navbar-collapse").collapse("hide");
+
+
+
+});
+
+    $('#product-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+    $('#testimonial-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    })
+
+})
+
